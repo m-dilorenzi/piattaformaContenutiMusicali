361404aebe8be8f47797be8d61f22c039a066c62
Matteo Di Lorenzi <matteo.dilorenzi98@gmail.com>
2019-06-28 18:25:46 +0200

    Aggiunta funzione per mandare un messaggio all'utente. Prima implementazione, per cui si potrà mandare all'utente solamente un semplice messaggio.

--- server.js
+++ server.js
@@ -11,17 +11,33 @@ const https = require('https');
 
 // Webhook per Telegram
 app.post('/telegram', (req, res) => {
-  console.log("Richiesta: " + JSON.stringify(req.body));
+	console.log("Richiesta: " + JSON.stringify(req.body));
+	const chatid = req.body.message.chat.id;
+	const text = req.body.message.text;
+	
+	console.log("Utente in chat " + chatid + " ha scritto '" + text + "'");
+	
+	res.end();
   
-  const chatid = req.body.message.chat.id;
-  const text = req.body.message.text;
-  
-  console.log("Utente in chat " + chatid + " ha scritto '" + text + "'");
+});
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+function sendText(chatId, text){
+  const requestBody = { 
+	      chat_id: chatId,
+	      text: text
+  }
   
   const clientreq = https.request({
     method: 'POST',
     host: 'api.telegram.org',
-    path: '/bot' + process.env.BOTTOKEN + '/getMe'
+    path: '/bot' + process.env.BOTTOKEN + '/sendMessage',
+    headers: {
+	    'Content-Type':'application/json',
+    },	  
   }, function(resp) {
     // Questa funzione viene richiamata a richiesta eseguita
     if(resp.statusCode != 200) {
@@ -29,24 +45,12 @@ app.post('/telegram', (req, res) => {
       return;
     }
     console.log("Richiesta HTTP riuscita");
-    
-    var body = '';
-    resp.on('data', function(d) {
-        body += d;
-    });
-    resp.on('end', function() {
-      // Ora body contiene il contenuto (corpo) della risposta
-      console.log("Risposta da API Telegram: " + body);
-      
-      const j = JSON.parse(body);
-      // j è un oggetto JavaScript che contiene i dati della risposta
-      // ...
-    });
+       
   });
+  clientreq.write(JSON.stringify(requestBody));	
   clientreq.end(); // questa chiamata esegue la richiesta
-  
-});
+}
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// link alle API esterne:
+// https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/#legal
+
