7767062f97caa39b1911bd351d2ea6d1d747b406
Matteo Di Lorenzi <matteo.dilorenzi98@gmail.com>
2019-08-28 14:45:21 +0200

    Cambio architettura della piattaforma. Ora il file si occuperà solo delle richieste provenienti dal WebService in ascolto dei messaggi di Telegram per provvedere poi a reperire i corretti risultati e ad inviarli come risposta.

--- server.js
+++ server.js
@@ -10,129 +10,70 @@ app.use(bodyparser.json());
 // Includiamo il modulo "request" per effettuare richieste HTTP
 const https = require('https');
 
-// Webhook per Telegram
-app.post('/telegram', (req, res) => {
-	console.log("Richiesta: " + JSON.stringify(req.body));
-	const chatid = req.body.message.chat.id;
-  const text = req.body.message.text;
-  const clientid = req.body.message.from.id;
-	
-	console.log("Utente in chat " + chatid + " ha scritto '" + text + "'");
-  
-  process.env.COMMAND_OR_INPUT = 1;
-  
-  if(text == "/start"){
-    sendText(chatid, "Benvenuto nel bot. Digita il comando /help per visualizzare i possibili comandi che il bot mette a disposizione.");
-    process.env.COMMAND_OR_INPUT = 0;
-    process.env.ACTION_TO_DO = 0;
-    checkTokenValidity(process.env.SPOTIFY_ACCESS_TOKEN);
-  }
-  if(text == "/searchsongbyparameter"){
-    sendText(chatid, "Digita i termini con cui eseguire la ricerca");
-    process.env.COMMAND_OR_INPUT = 0;
-    process.env.ACTION_TO_DO = 1;
-    checkTokenValidity(process.env.SPOTIFY_ACCESS_TOKEN);
-  }
-  if(text == "/getartistpagebyname"){
-    sendText(chatid, "Digita il nome dell'autore");
-    process.env.COMMAND_OR_INPUT = 0;
-    process.env.ACTION_TO_DO = 2;
-    checkTokenValidity(process.env.SPOTIFY_ACCESS_TOKEN);
-  }
-  if(text == "/searchyoutubevideos"){
-    sendText(chatid, "Digita i termini della ricerca");
-    process.env.COMMAND_OR_INPUT = 0;
-    process.env.ACTION_TO_DO = 3;
-    checkTokenValidity(process.env.SPOTIFY_ACCESS_TOKEN);
-  }
-  if(text == "/searchsongonspotify"){
-    if(clientid == process.env.ADMIN_ID){
-      sendText(chatid, "Digita i termini della ricerca");
-      process.env.COMMAND_OR_INPUT = 0;
-      process.env.ACTION_TO_DO = 4;
-      checkTokenValidity(process.env.SPOTIFY_ACCESS_TOKEN);
-    }else{
-      sendText(chatid, "Funzione riservata all'utente admin.");
-      process.env.COMMAND_OR_INPUT = 0;
-      process.env.ACTION_TO_DO = 0;
-    }
-  }
+app.post('/searchiTunesSong', (req, res) => {
+  var text = req.body.text;
+  console.log("L'utente vuole cercare dei brani su iTunes");
+	var resultString;
+  getMusicByParameter(text, function(result){
+    resultString = result;
+    console.log(resultString);
+    res.end(JSON.stringify(resultString));
+  });
+});
 
-  if(text == "/help"){
-    showInformation(chatid);
-    process.env.COMMAND_OR_INPUT = 0;
-    process.env.ACTION_TO_DO = 0;
-    checkTokenValidity(process.env.SPOTIFY_ACCESS_TOKEN);
-  }
-  
-  if(process.env.COMMAND_OR_INPUT == 1){
-    if((process.env.ACTION_TO_DO == 1) || 
-       (process.env.ACTION_TO_DO == 2) ||
-       (process.env.ACTION_TO_DO == 3) ||
-       (process.env.ACTION_TO_DO == 4)){
-      if(process.env.ACTION_TO_DO == 1){
-        getMusicByParameter(chatid, text);
-        process.env.ACTION_TO_DO = 0;
-      }
-      if(process.env.ACTION_TO_DO == 2){
-        getArtistPageByName(chatid, text);
-        process.env.ACTION_TO_DO = 0;
-      }
-      if(process.env.ACTION_TO_DO == 3){
-        searchYoutubeVideos(chatid, text);
-        process.env.ACTION_TO_DO = 0;
-      }
-      if(process.env.ACTION_TO_DO == 4){
-        console.log("Eseguo la ricerca su Spotify");
-        process.env.ACTION_TO_DO = 0;
-        searchSongOnSpotify(chatid, text, process.env.SPOTIFY_ACCESS_TOKEN);
-      }
+app.post('/searchiTunesArtist', (req, res) => {
+  var text = req.body.text;
+  console.log("L'utente vuole cercare un artista su iTunes");
+  var resultString;
+	getArtistPageByName(text, function(result){
+    resultString = result;
+    res.end(JSON.stringify(resultString));
+  });
+});
+
+app.post('/searchYoutubeVideos', (req, res) => {
+  var text = req.body.text;
+  console.log("L'utente vuole cercare video su YouTube");
+  var resultString;
+	searchYoutubeVideos(text, function(initialResult){
+    if(initialResult != "Nessun risultato disponibile."){
+      searchVideoStatistics(initialResult, function(finalResult){
+        res.end(JSON.stringify(finalResult));
+      });
     }else{
-      sendText(chatid, "Comando non disponibile.");
+      res.end(JSON.stringify("Nessun risultato disponibile."));
     }
-  }
-	res.end();
-  
+  });
+});
+
+app.post('/searchSongOnSpotify', (req, res) => {
+  var text = req.body.text;
+  var token = req.body.token;
+  console.log("L'utente vuole cercare brani su Spotify");
+  var resultString;
+	searchSongOnSpotify(text, token, function(result){
+    res.end(JSON.stringify(result));
+  });
+});
+
+app.post('/help', (req, res) => {
+  console.log("L'utente vuole eseguire il comando di help");
+  var resultString;
+	showInformation(function(result){
+    res.end(JSON.stringify(result));
+  });
 });
 
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-function sendText(chatId, text){
-  const requestBody = { 
-	      chat_id: chatId,
-	      text: text
-  }
-  
-  const clientreq = https.request({
-    method: 'POST',
-    host: 'api.telegram.org',
-    path: '/bot' + process.env.BOTTOKEN + '/sendMessage',
-    headers: {
-	    'Content-Type':'application/json',
-    },	  
-  }, function(resp) {
-    // Questa funzione viene richiamata a richiesta eseguita
-    if(resp.statusCode != 200) {
-      return;
-    }  
-  });
-  clientreq.write(JSON.stringify(requestBody));	
-  clientreq.end(); // questa chiamata esegue la richiesta
-}
-
-function getMusicByParameter(chatId, text){
-  
-  // console.log(text);
-  var searchString = text;
-  searchString = searchString.replace(/\s/g,"+");
-  // console.log(searchString);
-  
+function getMusicByParameter(text, result){
+    
   const clientreq = https.request({
     method: 'GET',
     host: 'itunes.apple.com',
-    path: '/search?term='+ searchString +'&attributeType=music&limit=10',
+    path: '/search?term='+ text +'&attributeType=music&limit=10',
     headers: {
 	    'Content-Type':'application/json',
     },	  
@@ -141,7 +82,7 @@ function getMusicByParameter(chatId, text){
     if(resp.statusCode != 200) {
       console.log("Richiesta HTTP iTunes fallita");
       console.log(resp.statusCode);
-      return;
+      return "Errore...";
     }
     console.log("Richiesta HTTP iTunes riuscita");
     
@@ -152,7 +93,7 @@ function getMusicByParameter(chatId, text){
     resp.on('end', function() {
       
       const j = JSON.parse(body);
-      // console.log(j);
+      //console.log(j);
       var string = '';
       if(j.resultCount == 0)
         string += "Nessun risultato disponibile";
@@ -162,30 +103,24 @@ function getMusicByParameter(chatId, text){
           string += "\nTitolo: " + j.results[i].trackName;
           string += "\nAlbum:  " + j.results[i].collectionName;
           string += "\nAutore: " + j.results[i].artistName;
-          string += "\nPrezzo: " + j.results[i].trackPrice+ "?";
-		  string += "\nLink:   " + j.results[i].trackViewUrl;
+          string += "\nPrezzo: " + j.results[i].trackPrice+ "€";
+		      string += "\nLink:   " + j.results[i].trackViewUrl;
           string += "\n";
         }
       }
-      sendText(chatId, string);
+      result(string);
     });
   });
-	
   clientreq.end(); // questa chiamata esegue la richiesta
-
 }
 
-function getArtistPageByName(chatId, text){
-  
-  // console.log(text);
-  var searchString = text;
-  searchString = searchString.replace(/\s/g,"+");
-  // console.log(searchString);
+
+function getArtistPageByName(text, result){
   
   const clientreq = https.request({
     method: 'GET',
     host: 'itunes.apple.com',
-    path: '/search?term='+ searchString +'&attributeType=allArtist&entity=allArtist&limit=5',
+    path: '/search?term='+ text +'&attributeType=allArtist&entity=allArtist&limit=5',
     headers: {
 	    'Content-Type':'application/json',
     },	  
@@ -194,7 +129,7 @@ function getArtistPageByName(chatId, text){
     if(resp.statusCode != 200) {
       console.log("Richiesta HTTP iTunes fallita");
       console.log(resp.statusCode);
-      return;
+      return "Errore...";
     }
     console.log("Richiesta HTTP iTunes riuscita");
     
@@ -217,46 +152,18 @@ function getArtistPageByName(chatId, text){
           string += "\n";
         }
       }
-      sendText(chatId, string);
+      result(string);
     });
   });
-	
   clientreq.end(); // questa chiamata esegue la richiesta
-
 }
 
-
-function showInformation(chatId){
-  var string = "Benvenuto nel bot FindYourFavouriteMusic!";
-  string += "\nI possibili comandi sono:";
-  string += "\n1. /searchsongbyparameter";
-  string += "\n  Permette di ricercare una canzone o una lista di canzoni tramite ";
-  string += "parametri come nome della canzone, artista, album, ecc.ecc. La ricerca ";
-  string += "puo' essere eseguita anche tramite un insieme di termini.";
-  string += "\n2. /getartistpagebyname";
-  string += "\n  Permette di ricercare la pagina iTunes di un cantante ";
-  string += "(o le pagine nel caso in cui i risultati della ricerca siano piu' di uno) ";
-  string += "alla quale si potra' poi accedere successivamente tramite l'apposito link ";
-  string += "che verra' mostrato.";
-  string += "\n3. /searchyoutubevideos";
-  string += "\n  Mostra i primi 5 video su YouTube che soddisfano ";
-  string += "i requisiti specificati nella ricerca.";
-  string += "\n4. /searchsongonspotify";
-  string += "\n  Mostra al piu' 5 canzoni con il rispettivo link di Spotify che ";
-  string += "soddisfano i requisiti specificati nella ricerca dall'utente.";
-  
-  sendText(chatId, string);
-}
-
-function searchYoutubeVideos(chatId, text) {
-  
-  var searchString = text;
-  searchString = searchString.replace(/\s/g,"+");
-  
+function searchYoutubeVideos(text, initialResult) {
+    
   const clientreq = https.request({
     method: 'GET',
     host: 'www.googleapis.com',
-    path: 'https://www.googleapis.com/youtube/v3/search?part=id&q='+searchString+'&type=video&maxResults=5'+
+    path: 'https://www.googleapis.com/youtube/v3/search?part=id&q='+text+'&type=video&maxResults=5'+
           '&key='+process.env.YOUTUBEKEY,
     headers: {
 	    'Content-Type':'application/json',
@@ -277,7 +184,7 @@ function searchYoutubeVideos(chatId, text) {
     resp.on('end', function() {
       
       const j = JSON.parse(body);
-      console.log(j);
+      //console.log(j);
       
       var searchStringStatistics = '';
       if(j.pageInfo.totalResults != 0){
@@ -286,11 +193,10 @@ function searchYoutubeVideos(chatId, text) {
           if(i!=4)
             searchStringStatistics += "%2C+";
         }
-        console.log(searchStringStatistics);
-        searchVideoStatistics(chatId, searchStringStatistics);
+        initialResult(searchStringStatistics);
       }else{
 		    var string = "Nessun risultato disponibile."; 
-		    sendText(chatId, string);
+        initialResult(string);
       }
     });
   });
@@ -299,7 +205,7 @@ function searchYoutubeVideos(chatId, text) {
   
 }
 
-function searchVideoStatistics(chatId, text){
+function searchVideoStatistics(text, finalResult){
   console.log("Esecuzione richiesta statistiche video");
   const clientreq = https.request({
     method: 'GET',
@@ -337,13 +243,13 @@ function searchVideoStatistics(chatId, text){
           string += "\n     Link: www.youtube.com/watch?v=" + j.items[i].id +" \n\n"
         }
       }
-      sendText(chatId, string);
+      finalResult(string);
     });
   });
   clientreq.end(); // questa chiamata esegue la richiesta
 }
 
-function searchSongOnSpotify(chatId, text, token){
+function searchSongOnSpotify(text, token, result){
   var searchString = text;
   searchString = searchString.replace(/\s/g,"+");
   
@@ -381,71 +287,32 @@ function searchSongOnSpotify(chatId, text, token){
       }
       if(string=="Lista canzoni\n")
         string="Nessun risultato disponibile.";
-      sendText(chatId, string);
-    });
-  });
-  clientreq.end(); // questa chiamata esegue la richiesta
-}
-
-function getNewAccessToken(){
-  var bodyRequest = querystring.stringify({
-    grant_type: 'refresh_token',
-    refresh_token: process.env.SPOTIFYKEYREFRESH
-  });
-  
-  const clientreq = https.request({
-    method: 'POST',
-    host: 'accounts.spotify.com',
-    path: '/api/token',
-    headers: {
-      'Content-Type':'application/x-www-form-urlencoded',
-	    'Authorization':'Basic '+process.env.SPOTIFYBASE64ID
-    },	  
-  }, function(resp) {
-    // Questa funzione viene richiamata a richiesta eseguita
-    if(resp.statusCode != 200) {
-      console.log("Richiesta HTTP Spotify NewToken fallita");
-      console.log(resp.statusCode);
-      return;
-    }
-    console.log("Richiesta HTTP Spotify NewToken riuscita");
-    
-    var body = '';
-    resp.on('data', function(d) {
-        body += d;
-    });
-    resp.on('end', function() {
       
-      const j = JSON.parse(body);
-      var token = j.access_token;
-      process.env.SPOTIFY_ACCESS_TOKEN = token;
-      console.log("Nuovo token ottenuto.");
+      result(string);
+      
     });
   });
-  //console.log(clientreq);
-  clientreq.write(bodyRequest);
   clientreq.end(); // questa chiamata esegue la richiesta
-  
 }
 
-function checkTokenValidity(token){
-  process.env.LOCK = 1;
-  var searchString = "prova";
-  const clientreq = https.request({
-    method: 'GET',
-    host: 'api.spotify.com',
-    path: '/v1/search?q='+searchString+'&type=track&limit=5&access_token='+token,
-    headers: {
-	    'Content-Type':'application/json',
-    },	  
-  }, function(resp) {
-    // Questa funzione viene richiamata a richiesta eseguita
-    if(resp.statusCode != 200) {
-      getNewAccessToken();
-    }else{
-      console.log("Token valido.");
-      console.log(process.env.SPOTIFY_ACCESS_TOKEN);
-    }
-  });
-  clientreq.end(); // questa chiamata esegue la richiesta
+function showInformation(result){
+  var string = "Benvenuto nel bot FindYourFavouriteMusic!";
+  string += "\nI possibili comandi sono:";
+  string += "\n1. /searchsongbyparameter";
+  string += "\n  Permette di ricercare una canzone o una lista di canzoni tramite ";
+  string += "parametri come nome della canzone, artista, album, ecc.ecc. La ricerca ";
+  string += "puo' essere eseguita anche tramite un insieme di termini.";
+  string += "\n2. /getartistpagebyname";
+  string += "\n  Permette di ricercare la pagina iTunes di un cantante ";
+  string += "(o le pagine nel caso in cui i risultati della ricerca siano piu' di uno) ";
+  string += "alla quale si potra' poi accedere successivamente tramite l'apposito link ";
+  string += "che verra' mostrato.";
+  string += "\n3. /searchyoutubevideos";
+  string += "\n  Mostra i primi 5 video su YouTube che soddisfano ";
+  string += "i requisiti specificati nella ricerca.";
+  string += "\n4. /searchsongonspotify";
+  string += "\n  Mostra al piu' 5 canzoni con il rispettivo link di Spotify che ";
+  string += "soddisfano i requisiti specificati nella ricerca dall'utente.";
+  
+  result(string);
 }
